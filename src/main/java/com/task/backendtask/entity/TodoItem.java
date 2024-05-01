508a4e4a42878d8a5aaa9d9786c26f97e1d43540
Almuhannad <almuhannad.khalid@gmail.com>
2024-05-01 23:32:12 +0300

    created user and todo_item entities

---
+++ src/main/java/com/task/backendtask/entity/TodoItem.java
@@ -0,0 +1,43 @@
+package com.task.backendtask.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class TodoItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.PENDING;
+
+    @Column(nullable = false)
+    private LocalDate dueDate;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+
+    // TodoList foreign key here
+
+
+}
+
+enum Status {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED
+}
---
+++ src/main/java/com/task/backendtask/entity/User.java
@@ -0,0 +1,30 @@
+package com.task.backendtask.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+}
+
+
