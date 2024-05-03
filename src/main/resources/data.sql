50c8900c927ff027ce40ab797cabbd6574246657
Almuhannad Alotaibi <almuhannad.khalid@gmail.com>
2024-05-03 23:51:48 +0300

    fixed null values in created_at table when seeding data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,11 +9,11 @@ INSERT INTO todo_lists (title, user_id) VALUES ('Work Tasks', 2);
 INSERT INTO todo_lists (title, user_id) VALUES ('Workouts', 3);
 
 -- Insert items
-INSERT INTO todo_items (description, status, due_date,  todo_list_id) VALUES ('Clean the house', 'PENDING', '2024-12-20', 1);
-INSERT INTO todo_items (description, status, due_date,  todo_list_id) VALUES ('Wash the floor', 'PENDING', '2024-12-26', 1);
+INSERT INTO todo_items (description, status, due_date, created_at, todo_list_id) VALUES ('Clean the house', 'PENDING', '2024-12-20', CURRENT_TIMESTAMP(), 1);
+INSERT INTO todo_items (description, status, due_date, created_at, todo_list_id) VALUES ('Wash the floor', 'PENDING', '2024-12-26', CURRENT_TIMESTAMP(), 1);
 
-INSERT INTO todo_items (description, status, due_date,  todo_list_id) VALUES ('Prepare meeting notes', 'PENDING', '2024-12-22', 2);
+INSERT INTO todo_items (description, status, due_date, created_at, todo_list_id) VALUES ('Prepare meeting notes', 'PENDING', '2024-12-22', CURRENT_TIMESTAMP(), 2);
 
-INSERT INTO todo_items (description, status, due_date,  todo_list_id) VALUES ('Bench press', 'PENDING', '2024-12-25', 3);
-INSERT INTO todo_items (description, status, due_date,  todo_list_id) VALUES ('Deadlift', 'PENDING', '2024-12-25', 3);
+INSERT INTO todo_items (description, status, due_date, created_at, todo_list_id) VALUES ('Bench press', 'PENDING', '2024-12-25', CURRENT_TIMESTAMP(), 3);
+INSERT INTO todo_items (description, status, due_date, created_at, todo_list_id) VALUES ('Deadlift', 'PENDING', '2024-12-25', CURRENT_TIMESTAMP(), 3);
 
